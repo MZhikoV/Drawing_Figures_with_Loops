af5503f387a42f4540355ed7f2ab626dd5968a8f
MZhikoV <m.zhikov@gmail.com>
2023-11-17 16:53:48 +0200

    Update 10. Diamond.cpp

--- Drawing_Figures_with_Loops/10. Diamond/10. Diamond.cpp
+++ Drawing_Figures_with_Loops/10. Diamond/10. Diamond.cpp
@@ -14,8 +14,8 @@ int main()
         cout << "**";
     }
     else {
-        for (int i = 1; i <= (n + 1) / 2; i++) {
-            if (n % 2 == 0) {
+        if (n % 2 == 0) {
+            for (int i = 1; i <= (n + 1) / 2; i++) {
                 for (int j = 1; j <= n; j++) {
                     if (j == ((n / 2) - i + 1) || j == ((n / 2) + i)) {
                         cout << '*';
@@ -24,12 +24,24 @@ int main()
                         cout << '-';
                     }
                 }
+                cout << endl;
             }
+            for (int i = ((n + 1) / 2)+1; i <= n; i++) {
+                for (int j = n; j >=1; j--) {
+                    if (j == ((n / 2) + i) /*|| j == ((n / 2) + i)*/) {
+                        cout << '*';
+                    }
+                    else {
+                        cout << '-';
+                    }
+                }
+                cout << endl;
+            }
+        }
             else {
                 cout << "todo";
             }
             cout << endl;
-        }
 
 
     }
